db3021813b02b401214628c6d9c9d27d7ce33054
zhangye-learn <928860758@qq.com>
2020-10-29 22:17:17 +0800

    release

--- src/main/java/javase/FeiBoNaQie.java
+++ src/main/java/javase/FeiBoNaQie.java
@@ -1,7 +1,25 @@
 package javase;
 
+import java.util.Scanner;
+
 public class FeiBoNaQie {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入打印个数：");
+        int count = sc.nextInt();
+        int x = 1;
+        int y = 1;
+        int z = 0;
+        for (int i=1;i<=count;i++){
+             if (i==1||i==2){
+                 System.out.println(1 + "\t");
+             }else{
+                 z = x + y;
+                 x = y;
+                 y = z;
+                 System.out.println(z +"\t");
+             }
+        }
 
     }
 }
