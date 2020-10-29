07b11a69f1cfba7022564b819b614d87364ce857
zhangye-learn <928860758@qq.com>
2020-10-29 22:59:10 +0800

    release

--- src/main/java/javase/FeiBoNaQie.java
+++ src/main/java/javase/FeiBoNaQie.java
@@ -21,5 +21,17 @@ public class FeiBoNaQie {
              }
         }
 
+        for (int i =1;i<=10;i++){
+            System.out.println(getNumber(i) + "\t");
+        }
+
+    }
+
+    public static int getNumber(int number){
+        if(number ==1||number==2){
+            return 1;
+        }else{
+            return getNumber(number-1) + getNumber(number -2);
+        }
     }
 }
